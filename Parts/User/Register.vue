8b887e912b9dd409302a95c309b490b86aa4538c
GCZ-Zero <1203970284@qq.com>
2022-11-02 08:09:05 +0000

    6

--- Parts/User/Register.vue
+++ Parts/User/Register.vue
@@ -1,7 +1,14 @@
-:<template>
+:
+<template>
   <n-form>
     <n-form-item-row label="用户名">
-      <n-input :loading="nameLoading" v-model:value="username" :status="name" @change="change" placeholder="中文可用，emoji不可用">
+      <n-input
+        :loading="nameLoading"
+        v-model:value="username"
+        :status="name"
+        @change="change"
+        placeholder="中文可用，emoji不可用"
+      >
         <template #suffix>
           {{ nametip }}
         </template>
@@ -41,11 +48,10 @@ export default {
       console.log(req)
       this.nametip = req.data.message
       if (req.data.code !== 200) {
-        
       }
       this.nameLoading = false
-    }
-  }
+    },
+  },
 }
 </script>
 
