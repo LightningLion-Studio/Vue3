8069aeadc25a1dcc892de34255b443b089427d2c
GCZ-Zero <1203970284@qq.com>
2022-11-09 07:25:16 +0000

    6

---
+++ Pages/Apps.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="apps">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    if (this.$route.params.e == 'bilibili') {
+      
+    }
+  }
+}
+</script>
