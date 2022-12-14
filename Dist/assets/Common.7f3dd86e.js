04b43e07e2c30ffb9d226f8d9949149d0239b9fd
祥和农业服务器 <1203970284@qq.com>
2022-12-14 11:11:35 +0800

    666

---
+++ Dist/assets/Common.7f3dd86e.js
@@ -0,0 +1,1 @@
+import{r as t}from"./Follow.cf4c9e47.js";const e="/assets/bilibili.0d2f45fd.png";async function s(n){return await t({method:"get",url:"/app",params:{name:n}})}async function r(){return await t.get("/announcement")}export{r as G,e as _,s as a};
--- Dist/assets/bilibili.ae784eb9.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/bilibili.0d2f45fd.png";export{s as _};
